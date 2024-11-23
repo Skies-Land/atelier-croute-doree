67678c2605e85769e5ff16d472ebe1ec36a36c5a
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-11-23 20:48:38 +0100

    Ajout d'animations de défilement avec ScrollReveal, mise à jour des styles CSS pour les media queries. Ajout de quelques balises pour améliorer l'accessibilité.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,8 @@ const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
-    // Lorsque l'utilisateur clique sur chaque "nav__link", 
-    // suppression de la class "show-menu"
+    // When the user clicks on each “nav__link”, 
+    // delete the “show-menu” class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -76,4 +76,18 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    // reset: true // Animation repeat
+})
 
+sr.reveal(`.home__data, .popular__container, .footer`);
+sr.reveal(`.home__board`, {delay: 700, distance: '100px', origin: 'right'});
+sr.reveal(`.home__pizza`, {delay: 1400, distance: '100px', origin: 'bottom', rotate: {z: -90}});
+sr.reveal(`.home__ingredient`, {delay: 2000, interval: 100});
+sr.reveal(`.about__data, .recipe__list, .contact__data`, {origin: 'right'});
+sr.reveal(`.about__img, .recipe__img, .contact__image`, {origin: 'left'});
+sr.reveal(`.products__card`, {interval: 100});
